d9adaff6c997d94df12cdb5de3cd7b8e7b329b47
Loshchilov Ilya <inik042311@gmail.com>
2025-11-12 11:27:25 +0300

    Initial Commit

---
+++ networkScannerApp/networkScannerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  networkScannerAppApp.swift
+//  networkScannerApp
+//
+//  Created by Илья Лощилов on 12.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct networkScannerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
